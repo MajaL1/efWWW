cbb05f41f88f1156cc382b256577c9b2ef2ed9c5
MajaL1 <majalokar@gmail.com>
2017-10-15 20:25:37 +0200

    Dodani novi fajli

--- public/js/controllers/MusicCtrl.js
+++ public/js/controllers/MusicCtrl.js
@@ -2,21 +2,37 @@
     console.log('Music controller');
 
     $scope.audioSources = [];
+    $scope.audioSingleList = [];
 
     $scope.loadAudios = function() {
 
-    	var audioDataList = [ {"title": "Izlet", "url": "assets/music/test4.mp3"},
-    						  {"title": "Sposti se", "url": "assets/music/test4.mp3"},
-    						  {"title": "Kaplja", "url": "assets/music/test4.mp3"},
-    						  {"title": "Mesto jekla in narcis", "url": "assets/music/test4.mp3"}
+    	var audioDataList = [ {"title": "Upanje", "url": "assets/music/Skarabeji - Upanje.mp3"},
+    						  {"title": "Vrtimo se", "url": "assets/music/Skarabeji - Vrtimo se.mp3"},
+    						  {"title": "Lep sončen dan (LSD)", "url": "assets/music/Skarabeji - Lep sončen dan.mp3"},
+    						  {"title": "Odšla sta v noč", "url": "assets/music/Skarabeji - Odsla sta v noc.mp3"},
+                              {"title": "To ni navaden dan", "url": "assets/music/Skarabeji - To ni navaden dan.mp3"},
+                              {"title": "Zapornik", "url": "assets/music/Skarabeji - Zapornik.mp3"},
+                              {"title": "Rock", "url": "assets/music/Skarabeji - Rock.mp3"},
+                              {"title": "Drugo dejanje", "url": "assets/music/Skarabeji - Drugo dejanje.mp3"},
+                              {"title": "Sam", "url": "assets/music/Skarabeji - Sam.mp3"},
+                              {"title": "Solza", "url": "assets/music/Skarabeji - Solza.mp3"},
+                              {"title": "Gibaj sm pa ke", "url": "assets/music/Skarabeji - Gibaj sm pa ke.mp3"}
     	 ];
 
 		for (i=0; i<audioDataList.length; i++){
 	    	$scope.audioSources.push(audioDataList[i]);
 		}
+
+        var audioSingleSourcesList = [ {"title": "Želja", "url": "assets/music/Zelja.mp3"},
+                              {"title": "Kaplja", "url": "assets/music/Kaplja.mp3"}
+         ];
+
+        for (i=0; i<audioSingleSourcesList.length; i++){
+            $scope.audioSingleList.push(audioSingleSourcesList[i]);
+        }
 	  
 
-	    console.log('------ audioSources: ', $scope.audioSources);
+	    console.log('------ audiosingleSources: ', $scope.audioSingleList);
 	}();
 
 });
--- public/js/controllers/VideoCtrl.js
+++ public/js/controllers/VideoCtrl.js
@@ -1,58 +1,57 @@
-﻿myApp.controller('VideoCtrl', function ($scope, $state, $stateParams) {
-    console.log('Video controller');
-
-    const YOUTUBE_URL = 'https://www.youtube.com/embed/';
-    /*** TODO: pridobi listo video-v iz youtube kanala ***/
-
-    $scope.videoSources = [];
 
 
-    $scope.loadVideos = function() {
+var key = "AIzaSyD7M2bnCmImthSaTjJEwOqo8PqsSkTK4GQ";
+var playlistId = "UULk_WVpcnHJMVAR2Fzg9FkA";
 
-    	/*var videoDataList = [ {"title": "Novi videospot Izlet", "url": "https://www.youtube.com/embed/ciXcjzHimjs"},
-    						  {"title": "Snemanje za nov komad 'Sposti se'", "url": "https://www.youtube.com/embed/pcEHZIQJTZk"},
-    						  {"title": "Utrinki iz koncerta v Vopa baru Kranjska gora", "url": "https://www.youtube.com/embed/iCFaep-7GW8"},
-    						  {"title": "Nov videospot", "url": "https://www.youtube.com/embed/KDMuK_9KtaE"}
-    	 ];
-		for (i=0; i<videoDataList.length; i++){
-	    	$scope.videoSources.push(videoDataList[i]);
-		}
-	    console.log('videosources: ', $scope.videoSources); */
+const YOUTUBE_URL = 'https://www.youtube.com/embed/';
 
-	   
-		    var apiKey = 'AIzaSyD7M2bnCmImthSaTjJEwOqo8PqsSkTK4GQ';
 
-		    gapi.client.setApiKey(apiKey);
-		    gapi.client.load('youtube', 'v3', function() {
+myApp.factory('youtubeService', function ($http) {
+    return {
+        getPlaylistVideos: function(playListId) {
+        return  $http.get('https://www.googleapis.com/youtube/v3/playlistItems', {params :{ part: 'snippet', maxResults: 10, playlistId: playlistId, key: key }});       
+    }
+}                
+});
 
-	        request = gapi.client.youtube.search.list({
-	            part: 'snippet',
-	            channelId: 'UCLk_WVpcnHJMVAR2Fzg9FkA',
-	            order: 'date',
-	            type: 'video'
 
-        	});
+myApp.controller('VideoCtrl', ['$http', '$scope', 'youtubeService', function ($http, $scope, youtubeService) {
 
-        request.execute(function(response) {
-                console.log('response: ', response);
-                let itemsList = response.items;
+        $scope.playlistVideos = [];
+        $scope.videoSources = [];
+
+        console.log('Start VideoCtrl');
+
+        var promise = youtubeService.getPlaylistVideos(playlistId);
+
+        promise.then(onSuccess, onError);
+
+
+        function onSuccess(event){
+
+            // QH3CgebBKrE
+            // https://www.youtube.com/watch?v=QH3CgebBKrE
+
+            let itemsList = event.data.items;
 
                 for(index in itemsList){
-                	let title = itemsList[index].snippet.title;
-                	let url = YOUTUBE_URL+itemsList[index].id.videoId;
-                	let videoItem = {
-                		"title" : title,
-                		"url" : url
-                	};
-                	console.log('videoItem, ', videoItem);
-                	$scope.videoSources.push(videoItem);
-                }
-        });
-    });
+                    let title = itemsList[index].snippet.title;
+                    let url = YOUTUBE_URL+itemsList[index].snippet.resourceId.videoId;
+                    let videoItem = {
+                        "title" : title,
+                        "url" : url
+                    };
+                    $scope.videoSources.push(videoItem);
+            }
+        }
 
-	}
-	$scope.loadVideos();
+        function onError(error){
 
-	}
-);
+                  $log.error('failure loading playlist', error);
+        }
+        
+}]);
+
+
+
 

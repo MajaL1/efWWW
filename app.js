20b66185ab1814ac8cfab0eb7c98ee0159143a94
MajaL1 <majalokar@gmail.com>
2017-10-22 20:44:28 +0200

    Popravki

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/api/update-counter', function (req, res) {
 
 
 
-var server = app.listen("3333", () => {
+var server = app.listen(process.env.PORT || 3000, () => {
 
 		var host = server.address().address;
 		var port = server.address().port;

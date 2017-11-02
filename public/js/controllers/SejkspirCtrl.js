8d8d321e8eccfa0e4c8d65c81f2b0ab885de5cd9
MajaL1 <MajaL1@users.noreply.github.com>
2017-11-02 08:26:40 +0100

    Update SejkspirCtrl.js

--- public/js/controllers/SejkspirCtrl.js
+++ public/js/controllers/SejkspirCtrl.js
@@ -1,5 +1,5 @@
 ﻿myApp.controller('SejkspirCtrl', function ($scope, $state, $stateParams) {
 	var mainBodyDiv = angular.element( document.querySelector( '.main-body-div' ) );
-	mainBodyDiv.css('background-color','#fff9ad');
+	mainBodyDiv.css('background-color','#FFF7C8');
     console.log('seXpir band controller');
 });

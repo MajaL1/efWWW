22e4c48ab301879d26b01c9a25c6f3c7a08adfc4
MajaL1 <majalokar@gmail.com>
2017-11-28 16:01:05 +0100

    Little responsive enhancments

--- public/js/controllers/SejkspirCtrl.js
+++ public/js/controllers/SejkspirCtrl.js
@@ -1,5 +1,9 @@
 ﻿myApp.controller('SejkspirCtrl', function ($scope, $state, $stateParams) {
 	var mainBodyDiv = angular.element( document.querySelector( '.main-body-div' ) );
 	mainBodyDiv.css('background-color','#FFF7C8');
+
+	var sejkspirDiv = angular.element( document.querySelector( '.sejkspir-div' ) );
+	sejkspirDiv.css('margin-top', '0px');
+
     console.log('seXpir band controller');
 });

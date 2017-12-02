56aabd039bf65daadaf69add98a7e2800d21e180
MajaL1 <majalokar@gmail.com>
2017-12-02 20:33:21 +0100

    Added responsive

--- public/js/controllers/SejkspirCtrl.js
+++ public/js/controllers/SejkspirCtrl.js
@@ -1,8 +1,10 @@
 ﻿myApp.controller('SejkspirCtrl', function ($scope, $state, $stateParams) {
 	var mainBodyDiv = angular.element( document.querySelector( '.main-body-div' ) );
 	mainBodyDiv.css('background-color','#FFF7C8');
+	mainBodyDiv.css('width','100%');
 
 	var sejkspirDiv = angular.element( document.querySelector( '.sejkspir-div' ) );
 	sejkspirDiv.css('margin-top', '0px');
+	sejkspirDiv.css('width', '70%');
 
 });

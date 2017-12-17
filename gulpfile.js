8b0ae2caedd824ea97073ebe2d66325303d8dbe0
MajaL1 <majalokar@gmail.com>
2017-12-17 20:34:01 +0100

    Added some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var gulp = require('gulp');
  // Define base folders
 var src = 'src/';
 var dest = 'build/';
+
+
  // Include plugins
+
+var minifyCss = require('gulp-minify-css'); 
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
@@ -13,7 +17,7 @@ var notify = require('gulp-notify');
 
 gulp.task('serve', function () {
     gulp.src('public').pipe(webserver({
-        port: 3333,
+        port: 3000,
         livereload: true
     })).pipe(notify("Running webserver!"));
 });
@@ -22,4 +26,18 @@ gulp.task('watch', ['serve'], function () {
     //gulp.start(['scripts', 'move']);
     gulp.watch(['js/**/*.js'], ['scripts']);
     gulp.watch(['views/**/*.html'], ['move']);
+});
+
+// task
+gulp.task('minify-css', function () {
+    gulp.src('./public/css/home.css') // path to your file
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/dist/css'));
+});
+
+// task
+gulp.task('minify-js', function () {
+    gulp.src('./public/js/controllers/*.js') // path to your files
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
 });

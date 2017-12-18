1cc7574132fbf20e400e605ad5e48c4190647e36
MajaL1 <MajaL1@users.noreply.github.com>
2017-12-18 14:28:19 +0100

    Update ConcertsCtrl.js

--- public/js/controllers/ConcertsCtrl.js
+++ public/js/controllers/ConcertsCtrl.js
@@ -1,4 +1,2 @@
 ﻿myApp.controller('ConcertsCtrl', function ($scope, $state, $stateParams) {
-    console.log('Koncerti controller');
-    $scope.data = 'ååååååååååååå   Tukaj mora biti OTHER view';
 });

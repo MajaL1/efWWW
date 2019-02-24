c0c18d6671b51fa5db1cdbedb5d192dba4736d99
Majalok <majalokar@gmail.com>
2019-02-24 10:53:23 +0100

    'changes..'

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
  // Define base folders
 var src = 'src/';
 var dest = 'build/';
-
+var flatten = require('gulp-flatten'); 
 
  // Include plugins
 
@@ -11,19 +11,36 @@ var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+
 var webserver = require('gulp-webserver');
 
 var notify = require('gulp-notify');
 
 gulp.task('serve', function () {
-    gulp.src('public').pipe(webserver({
+    gulp.src('dist').pipe(webserver({
         port: 3000,
         livereload: true
     })).pipe(notify("Running webserver!"));
 });
+gulp.task('scripts', function () {
+    gulp.src(['public/main.js', 'public/**/*.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(notify("JavaScript compiled!"));
+});
+
+gulp.task('move', function () {
+    gulp.src(['./public/index.html'])
+        .pipe(gulp.dest('./dist'));
+
+    gulp.src(['!./public/index.html', './public/**/*.html'])
+        .pipe(flatten())
+        .pipe(gulp.dest('./dist/templates'))
+        .pipe(notify("Moved HTML files!"));
+});
 
 gulp.task('watch', ['serve'], function () {
-    //gulp.start(['scripts', 'move']);
+    gulp.start(['scripts', 'move']);
     gulp.watch(['js/**/*.js'], ['scripts']);
     gulp.watch(['views/**/*.html'], ['move']);
 });
